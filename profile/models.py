517ce95ae2578d73d5d3bf4c2d5a808bada4034c
Alekxandr <alkv84@yandex.ru>
2017-12-06 22:28:41 +0200

    Add a few fields to profile model

--- profile/models.py
+++ profile/models.py
@@ -2,10 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.crypto import get_random_string
+from django.utils.translation import ugettext as _
+
+
+def set_profile_image_name(instanse, filename):
+    name = get_random_string(40)
+    ext = filename.split('.')[-1]
+    path = 'images/profile/{}.{}'.format(name, ext)
+    return path
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(verbose_name=_('Avatar'),
+                               upload_to=set_profile_image_name, blank=True, null=True, default=None)
+    birthday = models.DateField(verbose_name=_('Birthday'), blank=True, null=True, default=None)
 
 
 @receiver(post_save, sender=User)
