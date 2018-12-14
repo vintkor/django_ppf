6ba7c631e2c7f8115ab6ceea54ec51650a8de3c2
Alekxandr <alkv84@yandex.ru>
2018-12-14 16:50:32 +0800

    .

--- django_ppf/celery.py
+++ django_ppf/celery.py
@@ -9,17 +9,9 @@ app = Celery('django_ppf')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-app.conf.beat_schedule = {
-    'update_horoz_task_every_day': {
-        'task': 'assistant.update_horoz_task',
-        'schedule': crontab(hour=8, minute=24)
-    },
-    # 'blockio_transfer_usd_to_user_balance-each-5-minutes': {
-    #     'task': 'finance.blockio_transfer_usd_to_user_balance_task',
-    #     'schedule': crontab(minute='*/1')
-    # },
-    # 'points-awars-each-thursday-in-6h-10m': {
-    #     'task': 'award.start_point_awadr_task',
-    #     'schedule': crontab(hour=6, minute=10, day_of_week=2)
-    # },
-}
+# app.conf.beat_schedule = {
+#     'update_horoz_task_every_day': {
+#         'task': 'assistant.update_horoz_task',
+#         'schedule': crontab(hour=8, minute=24)
+#     },
+# }
