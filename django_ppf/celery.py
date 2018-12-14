0d0f7690faa97aacee54b0d6daaa58bd270feb45
Alekxandr <alkv84@yandex.ru>
2018-12-14 16:15:45 +0800

    .

--- django_ppf/celery.py
+++ django_ppf/celery.py
@@ -1,17 +1,18 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ppf.settings')
 
 app = Celery('django_ppf')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.beat_schedule = {
     'update_horoz_task_every_day': {
         'task': 'assistant.update_horoz_task',
-        'schedule': crontab(hour=8, minute=4)
+        'schedule': crontab(hour=8, minute=18)
     },
     # 'blockio_transfer_usd_to_user_balance-each-5-minutes': {
     #     'task': 'finance.blockio_transfer_usd_to_user_balance_task',
