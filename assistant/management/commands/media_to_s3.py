bc71863afdea8a886386d10bd4acb998e762a911
vintkor <alkv84@gmail.com>
2020-04-06 23:02:37 +0300

    Media to s3

--- assistant/management/commands/media_to_s3.py
+++ assistant/management/commands/media_to_s3.py
@@ -2,61 +2,54 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.files import File
 from django.conf import settings
 from django.apps import apps
+import re
+from django_ppf.storage_backends import PublicMediaStorage
 
 
 class Command(BaseCommand):
     """
-        python manage.py catalog.Category image
-        python manage.py catalog.Manufacturer logo
-        python manage.py catalog.Product image
-        python manage.py catalog.Benefit image
-        python manage.py catalog.Gallery image
-        python manage.py catalog.Document file
-        python manage.py catalog.Color image
-
-        python manage.py assistant.Product image
-        python manage.py assistant.Feature file
-        python manage.py assistant.Photo image
-
-        python manage.py company.Gallery image
-
-        python manage.py geo.Region image
-        python manage.py geo.ObjectImage image
-
-        python manage.py library.Document file
-
-        python manage.py news.News image
-        python manage.py news.Promo image
-        python manage.py news.Promo small_image
-
-        python manage.py partners.File file
-
-        python manage.py solutions.SolProduct image
-        python manage.py solutions.SolOffer image
-
-        python manage.py user_profile.Profile avatar
+        python manage.py media_to_s3 catalog.Product description
+        python manage.py media_to_s3 catalog.Product description_ru
+        python manage.py media_to_s3 catalog.Product description_uk
+        python manage.py catalog.Product use
+        python manage.py catalog.Product use_ru
+        python manage.py catalog.Product use_uk
     """
 
     def add_arguments(self, parser):
         parser.add_argument('app.model', type=str)
-        parser.add_argument('image_field', type=str)
+        parser.add_argument('field', type=str)
 
     def handle(self, *args, **options):
         app_name, model_name = options['app.model'].split('.')
         model = apps.get_model(app_label=app_name, model_name=model_name)
 
-        image_field = options['image_field']
+        field = options['field']
+        pattern = r'src=\"\/media\/(.*?)\" '
 
         for item in model.objects.all().iterator():
-            mediafile = getattr(item, image_field)
-            if not mediafile:
+            text = getattr(item, field)
+
+            if not text:
                 continue
 
-            filename = '{}/media/{}'.format(settings.BASE_DIR, mediafile)
-            try:
-                with open(filename, 'rb') as f:
-                    myfile = File(f)
-                    setattr(item, image_field, myfile)
-                    item.save()
-            except Exception as error:
-                print(error)
+            images = re.findall(pattern, text)
+            if not images:
+                continue
+
+            for image in images:
+                basename = '/media/{}'.format(image)
+                full_path = '{}{}'.format(settings.BASE_DIR, basename)
+
+                storage = PublicMediaStorage()
+
+                if not storage.exists(image):
+                    try:
+                        with open(full_path, 'rb') as f:
+                            storage.save(image, f)
+                    except Exception as error:
+                        print(error)
+
+                file_url = storage.url(image)
+                setattr(item, field, text.replace(basename, file_url))
+                item.save(update_fields=(field,))
